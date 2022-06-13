a38a7324b17027952f8b56a0a843659fb25b99ff
wucy <tianfudashuaichun@foxmail.com>
2022-06-13 11:05:02 +0800

    两道

--- src/leetcode/editor/cn/L155MinStack.java
+++ src/leetcode/editor/cn/L155MinStack.java
@@ -1,22 +1,25 @@
 package leetcode.editor.cn;
+
+import java.util.Stack;
+
 public class L155MinStack{
-    //设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+    //设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
 //
-// 实现 MinStack 类: 
+// 实现 MinStack 类:
 //
-// 
-// MinStack() 初始化堆栈对象。 
-// void push(int val) 将元素val推入堆栈。 
-// void pop() 删除堆栈顶部的元素。 
-// int top() 获取堆栈顶部的元素。 
-// int getMin() 获取堆栈中的最小元素。 
-// 
 //
-// 
+// MinStack() 初始化堆栈对象。
+// void push(int val) 将元素val推入堆栈。
+// void pop() 删除堆栈顶部的元素。
+// int top() 获取堆栈顶部的元素。
+// int getMin() 获取堆栈中的最小元素。
 //
-// 示例 1: 
 //
-// 
+//
+//
+// 示例 1:
+//
+//
 //输入：
 //["MinStack","push","push","push","getMin","pop","top","getMin"]
 //[[],[-2],[0],[-3],[],[],[],[]]
@@ -33,40 +36,45 @@ public class L155MinStack{
 //minStack.pop();
 //minStack.top();      --> 返回 0.
 //minStack.getMin();   --> 返回 -2.
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// -2³¹ <= val <= 2³¹ - 1 
-// pop、top 和 getMin 操作总是在 非空栈 上调用 
-// push, pop, top, and getMin最多被调用 3 * 10⁴ 次 
-// 
+//
+// 提示：
+//
+//
+// -2³¹ <= val <= 2³¹ - 1
+// pop、top 和 getMin 操作总是在 非空栈 上调用
+// push, pop, top, and getMin最多被调用 3 * 10⁴ 次
+//
 // 👍 1323 👎 0
 
     //leetcode submit region begin(Prohibit modification and deletion)
 class MinStack {
-
+    private Stack<Integer> minStack;
+    private Stack<Integer> stack;
     public MinStack() {
-
+        minStack = new Stack<>();
+        stack = new Stack<>();
+        minStack.push(Integer.MAX_VALUE);
     }
-    
+
     public void push(int val) {
-
+        stack.push(val);
+        minStack.push(Math.min(val, minStack.peek()));
     }
-    
+
     public void pop() {
-
+        stack.pop();
+        minStack.pop();
     }
-    
+
     public int top() {
-
+        return stack.peek();
     }
-    
-    public int getMin() {
 
+    public int getMin() {
+        return minStack.peek();
     }
 }
 
@@ -79,5 +87,5 @@ class MinStack {
  * int param_4 = obj.getMin();
  */
 //leetcode submit region end(Prohibit modification and deletion)
-   
+
 }
---
+++ src/leetcode/editor/cn/L15ThreeSum.java
@@ -0,0 +1,83 @@
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class L15ThreeSum{
+////给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
+// 请你找出所有和为 0 且
+//不重
+////复的三元组。
+////
+//// 注意：答案中不可以包含重复的三元组。
+////
+////
+////
+//// 示例 1：
+////
+////
+////输入：nums = [-1,0,1,2,-1,-4]
+////输出：[[-1,-1,2],[-1,0,1]]
+////
+////
+//// 示例 2：
+////
+////
+////输入：nums = []
+////输出：[]
+////
+////
+//// 示例 3：
+////
+////
+////输入：nums = [0]
+////输出：[]
+////
+////
+////
+////
+//// 提示：
+////
+////
+//// 0 <= nums.length <= 3000
+//// -10⁵ <= nums[i] <= 10⁵
+////
+//// 👍 4861 👎 0
+//
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 3) return res;
+        Arrays.sort(nums);
+        for (int i = 0, n = nums.length; i < n; i++) {
+            if (nums[i] > 0) break;
+            if (i>0&&nums[i] == nums[i - 1]) continue;//应该是判断与前一个
+            int l = i + 1;
+            int r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i] , nums[l] , nums[r]));
+                    while (l < r && nums[l] == nums[l+1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r-1]) {
+                        r--;
+                    }
+                    l++;r--;
+                } else if (sum > 0) {
+                    r--;
+                }else {
+                    l++;
+                }
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
