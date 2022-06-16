9b6fac34d945d42b2d336addf72dbb23e60ec9dd
wucy <tianfudashuaichun@foxmail.com>
2022-06-16 16:42:16 +0800

    树的遍历基础

---
+++ src/leetcode/editor/cn/L429NAryTreeLevelOrderTraversal.java
@@ -0,0 +1,90 @@
+package leetcode.editor.cn;
+
+import leetcode.editor.structure.Node;
+
+import java.util.*;
+
+public class L429NAryTreeLevelOrderTraversal{
+//给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
+//
+// 树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：root = [1,null,3,2,4,null,5,6]
+//输出：[[1],[3,2,4],[5,6]]
+//
+//
+// 示例 2：
+//
+//
+//
+//
+//输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,
+//null,13,null,null,14]
+//输出：[[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树的高度不会超过 1000
+// 树的节点总数在 [0, 10^4] 之间
+//
+// 👍 290 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        //BFS 广度优先，即每层都遍历
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                level.add(cur.val);
+                for (Node child : cur.children) {
+                    queue.offer(child);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
